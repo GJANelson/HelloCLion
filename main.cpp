4e1a58b7f50cf593e0a4c68ad7d6e5b27f366ac2
Garth Nelson <GJANelson@hotmail.com>
2018-09-13 14:06:00 -0700

    fixed header file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include "gcd.hpp"
 
+int gcd(const int a, const int b) {
+    if (a == 0){
+        return b;
+    } else {
+    return gcd(b % a, a);
+    }
+}
+
 int main() {
     std::cout << gcd(10, 25) << std::endl;
     return 0;
